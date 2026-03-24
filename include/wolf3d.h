4fc0d09bacc5272e34aee8451c61c7818450ff9a
mamadou <lucyyyy2004@gmail.com>
2026-03-24 23:40:57 +0100

    [Feat][Add] Started creating wolf3d's main and added main typedefs to the header.

---
+++ include/wolf3d.h
@@ -0,0 +1,52 @@
+/*
+** EPITECH PROJECT, 2025
+** wolf3d
+** File description:
+** Main header file of wolf3d
+*/
+
+#ifndef WOLF3D_H
+    #define WOLF3D_H
+
+    // Standard includes
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <stdbool.h>
+    #include <unistd.h>
+    #include <math.h>
+
+    // Custom includes
+    #include "./../libs/c_alloc/c_alloc.h"
+    #include "./../libs/linkedlist/linkedlist.h"
+    #include "./../libs/str/str.h"
+    #include "./../libs/buttonfml/buttonfml.h"
+    #include "./../libs/setfml/setfml.h"
+    #include "./../libs/raycaster/include/raycaster.h"
+    #include "./../libs/classhandler/include/classhandler.h"
+
+    // Return codes
+    #define WOLF_FAIL 0
+    #define WOLF_SUCC 84
+
+// Typedefs
+
+typedef enum gamestate_e {
+    GAME_MENU,
+    GAME_SETTINGS,
+    GAME_PLAY,
+    GAME_PAUSED,
+} gamestate_t;
+
+typedef struct wolf_s {
+    c_alloc_t *alloc;
+    setfml_t *setfml;
+    buttonfml_t *buttonfml;
+    char **map;
+    gamestate_t state;
+} wolf_t;
+
+// Functions
+
+
+
+#endif
--- src/wolf3d.c
+++ src/wolf3d.c
@@ -5,7 +5,107 @@
 ** Main entry point of wolf3d.
 */
 
-int main(int argc, char **argv)
+#include "./../include/wolf3d.h"
+
+// Fonction temporaire pour faire une map vu que je connais pas les consignes
+char **create_map(void)
+{
+    const char *rows[] = {
+        "111111111111111111111111111111",
+        "100000000000000000000000000001",
+        "100000000000000000000000000001",
+        "100011110000000001111000000001",
+        "100010000000000000001000000001",
+        "100010001111111100001000000001",
+        "100010001000000100001000000001",
+        "100010001000000100001111110001",
+        "100000001000000100000000000001",
+        "100000001000000100000000000001",
+        "100011111000000111110000000001",
+        "100000000000000000000000000001",
+        "100000000000000000000000000001",
+        "100000000111000000000111000001",
+        "100000000010000000000010000001",
+        "100000000010000000000010000001",
+        "100111100010001110000010000001",
+        "100000000010000010000010000001",
+        "100000000000000010000000000001",
+        "100000000000000010000000000001",
+        "100000001111111110000111111001",
+        "100000000000000000000000000101",
+        "100000000000000000000000000101",
+        "101111000000111111000000001101",
+        "100001000000100001000000001001",
+        "100001000000100001000000000001",
+        "100001111111100001111111000001",
+        "100000000000000000000000000001",
+        "100000000000000000000000000001",
+        "111111111111111111111111111111",
+        NULL
+    };
+    int row_count = 0;
+    char **map = NULL;
+
+    for (; rows[row_count]; row_count++);
+    map = malloc(sizeof(char *) * (row_count + 1));
+    if (!map)
+        return NULL;
+    for (int i = 0; i < row_count; i++) {
+        map[i] = malloc(sizeof(char) * (str_len(rows[i]) + 1));
+        if (!map[i])
+            return NULL;
+        str_cpy(rows[i], map[i]);
+    }
+    map[row_count] = NULL;
+    return map;
+}
+
+// Même chose
+static void free_map(char **map)
+{
+    for (int i = 0; map[i]; i++)
+        free(map[i]);
+    free(map);
+}
+
+static int ini_csfml(wolf_t *wolf)
 {
     return 0;
+}
+
+static int ini_map(wolf_t *wolf)
+{
+    return 0;
+}
+
+static wolf_t *ini_main(c_alloc_t *alloc)
+{
+    wolf_t *wolf = NULL;
+
+    if (!alloc)
+        return NULL;
+    wolf = c_alloc(sizeof(wolf_t), 1, alloc);
+    if (!wolf) {
+        c_delete(alloc, false);
+        return NULL;
+    }
+    wolf->alloc = alloc;
+    wolf->map = create_map();
+    if (!wolf->map) {
+        c_delete(alloc, true);
+        return NULL;
+    }
+    return wolf;
+}
+
+int main(void)
+{
+    c_alloc_t *alloc = c_ini((uint16_t)100);
+    wolf_t *wolf = ini_main(alloc);
+
+    if (!wolf)
+        return WOLF_FAIL;
+    free_map(wolf->map);
+    c_delete(alloc, true);
+    return WOLF_SUCC;
 }
